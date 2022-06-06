0d8cd0d2423a4fede503075deed018d9a4a58923
GiovannaMorais <gigimorais9@hotmail.com>
2022-06-06 15:17:38 -0300

    REQUISITO 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando o componente App', () => {
+  it('Testando o primeiro link do topo da aplicação', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const firstLink = screen.getByRole('link', { name: /home/i });
+    expect(firstLink).toBeInTheDocument();
+
+    userEvent.click(firstLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Testando o segundo link do topo da aplicação', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const secondLink = screen.getByRole('link', { name: /about/i });
+    expect(secondLink).toBeInTheDocument();
+
+    userEvent.click(secondLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Testando o terceiro link do topo da aplicação', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const thirdLink = screen.getByText(/Favorite/i);
+    expect(thirdLink).toBeInTheDocument();
+
+    userEvent.click(thirdLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Testando se a aplicação é redirecionada para a página Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/not-found');
+    const NotFound = screen.getByText('Page requested not found');
+    expect(NotFound).toBeInTheDocument();
+  });
+});
